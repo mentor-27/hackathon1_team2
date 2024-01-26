e55fd57f3dac022f375a999e9a637591cd88a321
mentor-27 <mexanoid2012@gmail.com>
2024-01-26 21:14:51 +0200

    preset for main app start

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+  }
 
+  init() {
+    document.addEventListener('contextmenu', event => {
+      event.preventDefault();
+    });
+  }
+
+  open() {
+    this.el.classList.add('open');
+  }
+
+  close() {
+    this.el.classList.remove('open');
+  }
+
+  add(module) {
+    this.el.insertAdjacentHTML('beforeend', module.toHTML());
+  }
 }
