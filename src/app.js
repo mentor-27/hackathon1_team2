6577f4c3587db0771dee963c62ef3ccb3cba72f8
mentor-27 <mexanoid2012@gmail.com>
2024-01-26 21:45:21 +0200

    small fix in menu.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-import './styles.css'
+import { ContextMenu } from './menu';
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
+import './styles.css';
 
+// const modules = [BackgroundModule, ClicksModule, ShapeModule];
+const context = new ContextMenu('#menu');
+
+// modules.forEach(module => {
+//   context.add(module);
+// });
