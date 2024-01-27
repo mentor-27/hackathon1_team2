ae3d46772004780e76a70511e07e1005805e4696
mentor-27 <mexanoid2012@gmail.com>
2024-01-27 20:44:42 +0200

    code-style formatting

--- src/app.js
+++ src/app.js
@@ -8,15 +8,13 @@ import { RandomSound } from './modules/randomSound.module';
 import './styles.css';
 
 const customMessage = new CustomMessage('custom_message', 'Вызвать сообщение');
-const randomBackground = new BackgroundModule(
-
-  'random_background',
-  'Поменять цвет'
-);
+const randomBG = new BackgroundModule('random_background', 'Поменять цвет');
 const timer = new Timer('timer', 'Установить таймер');
 const randomSound = new RandomSound('random_sound');
 const clickAnalytics = new ClicksModule('clicks', 'Счетчик кликов');
-const modules = [customMessage, randomBackground, timer, randomSound, clickAnalytics];
+
+const modules = [customMessage, randomBG, timer, randomSound, clickAnalytics];
+
 const contextMenu = new ContextMenu('#menu');
 
 modules.forEach(module => {
