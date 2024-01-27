79b1238b675b5888d4ceef39ea28082ffd2ec261
Tkach Vladyslav <superovlad39@gmail.com>
2024-01-27 08:56:57 +0100

    ready-made timer functionality and style adjustments

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -6,35 +6,67 @@ export class Timer extends Module {
 	}
 
 	trigger() {
-		const block = document.createElement('div')
-		block.classList.add('custom-message', 'timer-block')
+		const isTimerBlock = document.querySelector('.timer-block')
+		if (!isTimerBlock) {
+			const block = document.createElement('div')
+			block.classList.add('custom-message', 'timer-block')
 
-		const form = document.createElement('form')
-		form.classList.add('timer-form')
+			const form = document.createElement('form')
+			form.classList.add('timer-form')
 
-		const startTimerBtn = document.createElement('button')
-		startTimerBtn.classList.add('timer-form__button')
-		startTimerBtn.textContent = 'Запустить таймер'
+			const startTimerBtn = document.createElement('button')
+			startTimerBtn.classList.add('timer-form__button')
+			startTimerBtn.textContent = 'Запустить таймер'
 
-		const input = document.createElement('input')
-		input.classList.add('timer-form__input')
-		input.min = 1
-		input.max = 1000
-		input.type = 'number'
-		input.placeholder = 'Введите нужное время в секундах'
+			const input = document.createElement('input')
+			input.classList.add('timer-form__input')
+			input.min = 1
+			input.max = 1000
+			input.type = 'number'
+			input.placeholder = 'Введите нужное время в секундах'
 
-		const timeDisplay = document.createElement('span')
-		timeDisplay.textContent = '0'
-		const timeDisplayBlock = document.createElement('div')
-		timeDisplayBlock.textContent = `Осталось ${timeDisplay.textContent} секунд`
+			const timeDisplayBlock = document.createElement('div')
+			timeDisplayBlock.id = 'timeDisplayBlock'
 
-		form.append(input, startTimerBtn)
-		block.append(form)
-		document.body.append(block)
+			form.append(input, startTimerBtn)
+			block.append(form)
+			document.body.append(block)
 
+			this.#startTimer(startTimerBtn, block, timeDisplayBlock, input)
+		}
+	}
+
+	#startTimer(startTimerBtn, block, timeDisplayBlock, input) {
 		startTimerBtn.addEventListener('click', event => {
 			event.preventDefault()
 			block.append(timeDisplayBlock)
+
+			const inputValue = input.value
+			let timer = Number(inputValue)
+
+			if (!timeDisplayBlock.textContent && inputValue) {
+				const timerWork = setInterval(() => {
+					timer--
+					this.#secondsDisplayOption(timer, timeDisplayBlock)
+
+					if (timer < 0) {
+						clearInterval(timerWork)
+						block.remove()
+					}
+				}, 1000)
+			}
 		})
+	}
+
+	#secondsDisplayOption(num, block) {
+		if (num % 10 >= 11 && num % 10 <= 19) {
+			block.textContent = `Осталось ${num} секунд`
+		} else if (num % 10 === 1) {
+			block.textContent = `Осталось ${num} секунда`
+		} else if (num % 10 >= 2 && num % 10 <= 4) {
+			block.textContent = `Осталось ${num} секунды`
+		} else {
+			block.textContent = `Осталось ${num} секунд`
+		}
 	}
 }
