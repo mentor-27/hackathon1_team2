241eb238e07c12494450035532d1e0907b4f6d72
Pavel <mymailppa@gmail.com>
2024-01-27 19:51:38 +0300

    Added clicks module

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-import { ContextMenu } from './menu';
-import { BackgroundModule } from './modules/background.module';
-import { ClicksModule } from './modules/clicks.module';
-import { CustomMessage } from './modules/customMessage.module';
-import { ShapeModule } from './modules/shape.module';
-import { Timer } from './modules/timer.module';
-import './styles.css';
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { CustomMessage } from "./modules/customMessage.module";
+import { ShapeModule } from "./modules/shape.module";
+import { Timer } from "./modules/timer.module";
+import "./styles.css";
 
-const customMessage = new CustomMessage('custom_message', 'Вызвать сообщение');
+const customMessage = new CustomMessage("custom_message", "Вызвать сообщение");
 const randomBackground = new BackgroundModule(
-	'random_background',
-	'Поменять цвет'
+  "random_background",
+  "Поменять цвет"
 );
-const timer = new Timer('timer', 'Установить таймер');
-const modules = [customMessage, randomBackground, timer];
-const contextMenu = new ContextMenu('#menu');
+const timer = new Timer("timer", "Установить таймер");
+const clicks = new ClicksModule("clicks", "Счетчик кликов");
+const modules = [customMessage, randomBackground, timer, clicks];
+const contextMenu = new ContextMenu("#menu");
 
-modules.forEach(module => {
-	contextMenu.add(module);
-})
+modules.forEach((module) => {
+  contextMenu.add(module);
+});
 
 contextMenu.init(modules);
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,66 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
 
+  trigger() {
+    const isTimerBlock = document.querySelector(".timer-block");
+
+    if (isTimerBlock) {
+      return;
+    }
+
+    const block = document.createElement("div");
+    block.classList.add("custom-message", "timer-block", "timer-message");
+
+    const startTimerBtn = document.createElement("button");
+    startTimerBtn.classList.add("timer-button");
+    startTimerBtn.textContent = "Запустить таймер";
+
+    const input = document.createElement("input");
+    input.classList.add("timer-input");
+    input.type = "text";
+    input.placeholder = "Значение в секундах";
+
+    input.addEventListener("input", (event) => {
+      const { target } = event;
+      target.value = target.value.replace(/\D/g, "");
+    });
+
+    const timeDisplayBlock = document.createElement("div");
+    timeDisplayBlock.id = "timeDisplayBlock";
+
+    block.append(input, startTimerBtn);
+    document.body.append(block);
+
+    startTimerBtn.addEventListener("click", () => {
+      let clickCount = -1;
+      let inputValue = Number(input.value);
+
+      if (!inputValue) {
+        return;
+      }
+
+      document.addEventListener("click", () => clickCount++);
+
+      input.replaceWith(timeDisplayBlock);
+      startTimerBtn.remove();
+      timeDisplayBlock.textContent = `Осталось ${inputValue} сек`;
+
+      const timerWork = setInterval(() => {
+        timeDisplayBlock.textContent = `Осталось ${--inputValue} сек`;
+
+        if (inputValue <= 0) {
+          clearInterval(timerWork);
+          const statsMessage = `Вы сделали ${clickCount} кликов за ${input.value} секунд(ы).`;
+          timeDisplayBlock.textContent = statsMessage;
+          setTimeout(() => {
+            block.remove();
+          }, 3e3);
+        }
+      }, 1000);
+    });
+  }
 }
