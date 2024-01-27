292fb4ccbd3a63400d6e534a433ccaf93513174a
Есиков <dedmaster.developer@yandex.ru>
2024-01-27 13:47:40 +0300

    Добавлены звуки

---
+++ src/data/soundPath.js
@@ -0,0 +1,11 @@
+export const soundArray = [
+    '../sound/1.waw',
+    '../sound/2.waw',
+    '../sound/3.waw',
+    '../sound/4.waw',
+    '../sound/5.waw',
+    '../sound/6.waw',
+    '../sound/7.waw',
+    '../sound/8.waw',
+    '../sound/9.waw',
+]
